0d7ec72973f49b992130b8e1e4872974e0d4ed1f
Sasi Koramutla <sasidhar.kmv@gmail.com>
2020-07-15 21:48:28 -0400

    updated index.js with jsx style script

--- react-essential/src/index.js
+++ react-essential/src/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const title = React.createElement(
+var style = {
+  backgroundColor: "orange",
+  color:"white",
+  fontFamily: "Arial"
+};
+
+/* const title = React.createElement(
   "h1",
-  {id: "title", className: "header"},
+  {id: "title", className: "header", style:style},
   "Hello World"
-);
+); */
 
 ReactDOM.render(
-  title,
+  <div style={style}>
+    <h1 id="heading-element">Hello World!</h1>
+    <p>Glad you are here!</p>
+  </div>,
   document.getElementById("root")
 );
